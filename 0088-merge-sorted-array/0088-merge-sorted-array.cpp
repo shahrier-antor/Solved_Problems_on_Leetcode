e56addc2b16e15a1ff19bdb9661974f937e9ba0b
MD KAF SHAHRIER <antor3036@gmail.com>
2024-12-08 04:03:52 +0200

    Time: 0 ms (100%), Space: 12.3 MB (6.26%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        nums1.erase(nums1.begin() + m, nums1.end());          
+        sort(nums2.begin(),nums2.end());
+        for(int i=0; i<nums2.size(); i++)
+        {
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());     
+    }
+};
