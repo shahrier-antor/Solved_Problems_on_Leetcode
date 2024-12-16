fdabd341bb62d9f7fbcda91da14092e50a0bbc38
Md Kaf Shahrier <antor3036@gmail.com>
2024-12-16 18:53:08 +0200

    Time: 0 ms (100%), Space: 59.2 MB (21.46%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,25 +1,44 @@
+// class Solution {
+// public:
+//     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+//         for(int i = 1; i<arr.size(); i++)
+//         {
+//             arr[i]+=arr[i-1];
+//         }
+//         int left = 0, right = k-1;
+//         int count = 0;
+//         while(right<arr.size()){
+//             int sum; 
+//             if(left == 0){
+//                 sum = arr[right];
+//             }
+//             else {
+//                 sum = arr[right] - arr[left-1];
+//             }
+//             if(sum>= k* threshold) count++;
+//             left++;
+//             right++;
+//         }
+//         return count;	
+//     }
+// };
+
+
 class Solution {
+    // T:O(N),S: O(1)
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-        for(int i = 1; i<arr.size(); i++)
-        {
-            arr[i]+=arr[i-1];
-        }
         int left = 0, right = k-1;
-        int count = 0;
+        int sum = 0, count = 0;
+        for(int i = 0; i<k; i++)sum+=arr[i];
         while(right<arr.size()){
-            int sum; 
-            if(left == 0){
-                sum = arr[right];
-            }
-            else {
-                sum = arr[right] - arr[left-1];
-            }
-            if(sum>= k* threshold) count++;
+
+            if(sum>= threshold*k)count++;
+            sum-=arr[left];
             left++;
             right++;
+            if(right<arr.size())sum+=arr[right];
         }
-        return count;	
+        return count;
     }
 };
-
