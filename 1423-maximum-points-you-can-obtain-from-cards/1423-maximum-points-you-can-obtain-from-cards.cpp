26c91ac0d6e0e1a6e131cbdb91ed047b11c1f466
Md Kaf Shahrier <antor3036@gmail.com>
2024-12-16 18:27:17 +0200

    Time: 0 ms (100%), Space: 46.2 MB (26.29%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,36 +1,3 @@
-// class Solution {
-// public:
-// // T: O(n), S: O(n)
-//     int maxScore(vector<int>& cardPoints, int k) {
-
-//         vector<int>preSum(cardPoints.size(),0);
-//         vector<int>suffSum(cardPoints.size(),0);
-
-//         preSum[0] = cardPoints[0];
-//         for(int i = 1; i<cardPoints.size(); i++)preSum[i] = preSum[i-1] + cardPoints[i];
-
-//         suffSum[0] = cardPoints[cardPoints.size()-1];
-//         int l = 1;
-//         for(int i = cardPoints.size()-2; i>=0; i--){
-//             suffSum[l] = suffSum[l-1] + cardPoints[i];
-//             l++;
-//         }
-//         int maxPoint = 0;
-//         int preValue = k-1;
-//         int suffValue = -1;
-//         while(preValue>=-1){
-//             int sum = 0;
-//             if(preValue>=0)  sum+=preSum[preValue];
-//             if(suffValue>=0) sum+= suffSum[suffValue];
-//             maxPoint = max(maxPoint, sum);
-//             suffValue++;
-//             preValue--;
-//         }
-//         return maxPoint;
-//     }
-// };
-
-
 
 class Solution {
 public:
@@ -41,7 +8,7 @@ public:
 
         int left = 0, right = cardPoints.size()-k;
         int sum = maximum;
-
+        
         while(right<cardPoints.size()){
             sum += cardPoints[left];
             sum -= cardPoints[right];
@@ -49,7 +16,6 @@ public:
             left++;
             right++;
         }
-    
         return maximum;
     }
 };
