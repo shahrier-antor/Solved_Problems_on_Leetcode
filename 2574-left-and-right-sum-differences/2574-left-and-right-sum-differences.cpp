486d42fe9e6829e03bb65089f2ab3a142a435ac2
Md Kaf Shahrier <antor3036@gmail.com>
2024-12-17 23:51:11 +0200

    Time: 0 ms (100%), Space: 15 MB (24.51%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
         vector<int> leftSum(nums.size(), 0), rightSum(nums.size(), 0), answer(nums.size(), 0);
-        for(int i=1; i<nums.size(); i++) leftSum[i] = leftSum[i-1]+nums[i-1];
-        for(int i=0; i<nums.size(); i++) cout<<leftSum[i]<<" ";
-        cout<<endl;
-        for(int i=nums.size()-2; i>=0; i--) rightSum[i] = rightSum[i+1]+nums[i+1];
-        for(int i=0;i<nums.size();i++) cout<<rightSum[i]<< " ";
-        cout<<endl;
-        for(int i=0; i<nums.size(); i++) answer[i]= abs(leftSum[i] - rightSum[i]);
+        int totalSum = 0;
+        for(int i = 0; i<nums.size();i++)totalSum += nums[i];
+        for(int i=1; i<nums.size(); i++) answer[i] = answer[i-1]+nums[i-1];
+        for(int i = 0; i<nums.size();i++){
+            int sum = totalSum;
+            sum-=answer[i];
+            sum-=nums[i];
+            answer[i] = abs(answer[i]-sum);
+        }
+        //for(int i=nums.size()-2; i>=0; i--) rightSum[i] = rightSum[i+1]+nums[i+1];
+        //for(int i=0; i<nums.size(); i++) answer[i]= abs(leftSum[i] - rightSum[i]);
         return answer;        
     }
 };
