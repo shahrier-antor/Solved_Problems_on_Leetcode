237d4d5a0594883df678faa1457c287520e82c78
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-05 23:57:30 +0200

    Time: 4 ms (33.62%), Space: 11 MB (9.59%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,14 +5,10 @@ public:
             return false;
         }
 
-        long reverse = 0;
-        int xcopy = x;
-
-        while (x > 0) {
-            reverse = (reverse * 10) + (x % 10);
-            x /= 10;
-        }
-
-        return reverse == xcopy;        
+        string str = to_string(x);  
+        string rev = str;
+        reverse(str.begin(), str.end());
+        if(str == rev) return true;
+        else return false;
     }
 };
