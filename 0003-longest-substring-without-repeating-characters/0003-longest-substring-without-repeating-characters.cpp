b914c83bfa977f956bed78da8454509698c8255a
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-12 16:04:20 +0200

    Time: 14 ms (45.15%), Space: 13.1 MB (40.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,53 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int a_pointer = 0; 
+//         int b_pointer = 0; 
+//         int max_length = 0; 
+//         unordered_set<char> hash_set; 
+
+//         while (b_pointer < s.length()) {
+//             if (hash_set.find(s[b_pointer]) == hash_set.end()) {
+//                 hash_set.insert(s[b_pointer]);
+//                 b_pointer++;
+//                 int size = hash_set.size();
+//                 max_length = max(size, max_length); 
+//             } else {
+//                 hash_set.erase(s[a_pointer]);
+//                 a_pointer++;
+//             }
+//         }
+//         return max_length;
+//     }
+// };
+
+// abcabcbb
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int a_pointer = 0; // Start pointer
-        int b_pointer = 0; // End pointer
-        int max_length = 0; // Maximum length of substring
-        std::unordered_set<char> hash_set; // Set to store unique characters
+        int low = 0, high=0, length = 0;
+        unordered_map<string,int>h_map;
 
-        while (b_pointer < s.length()) {
-            if (hash_set.find(s[b_pointer]) == hash_set.end()) {
-                // Add the character to the set and move the end pointer forward
-                hash_set.insert(s[b_pointer]);
-                b_pointer++;
-                max_length = max(static_cast<int>(hash_set.size()), max_length); // Update the max length
-            } else {
-                // Remove the starting character and move the start pointer forward
-                hash_set.erase(s[a_pointer]);
-                a_pointer++;
+        while(high<s.size()){
+            
+            string right = "";
+            right+=s[high];
+           // cout<< "input :"<< right<<"inputends"<<endl;
+            h_map[right]++;
+            
+            while(h_map[right]>1 && low<=high){
+             string left = "";
+             left+=s[low];
+             h_map[left]--;
+             low++;
             }
+            length = max(length,high-low+1); 
+             high++;
         }
 
-        return max_length;
+        return length;
+      
     }
 };
+
+
