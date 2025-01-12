4357e137a2f2fc9fb7663a99d65ba04c7e976e89
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-12 12:41:22 +0200

    Time: 165 ms (15.62%), Space: 67.4 MB (12.3%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,40 +1,3 @@
-// class Solution {
-// public:
-//     int subarraysWithKDistinct(vector<int>& nums, int k) {
-//         return atMostKDistinct(nums, k) - atMostKDistinct(nums, k - 1);
-//     }
-
-// private:
-//     int atMostKDistinct(const vector<int>& nums, int k) {
-//         int low = 0, high = 0, count = 0;
-//         unordered_map<int, int> h_map;
-
-//         while (high < nums.size()) {
-//             // Add nums[high] to the map
-//             h_map[nums[high]]++;
-//             high++;
-
-//             // Shrink the window if there are more than k distinct integers
-//             while (h_map.size() > k) {
-//                 h_map[nums[low]]--;
-//                 if (h_map[nums[low]] == 0) {
-//                     h_map.erase(nums[low]);
-//                 }
-//                 low++;
-//             }
-
-//             // Add the number of subarrays ending at high-1
-//             count += high - low;
-//         }
-
-//         return count;
-//     }
-// };
-
-
-// 1,2,1,2,3 k = 2 ;
-// 4 + 3 + 2 +
-
 class Solution{
 
 public:
@@ -48,7 +11,7 @@ private:
 
         while(high<nums.size()){
             h_map[nums[high]]++;
-            high++;
+          
 
             while(h_map.size()>k){
                 h_map[nums[low]]--;
@@ -58,6 +21,7 @@ private:
                 low++;
             }
             count = count+high-low; 
+            high++;
         }
         return count;
     }
