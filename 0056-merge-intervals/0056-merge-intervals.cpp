5565a2ee029f9aa8ce74c710b8d46301d94993b5
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-12 16:59:33 +0200

    Time: 7 ms (43.73%), Space: 24.7 MB (17.55%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>answer;
+
+        for(auto interval:intervals){
+            if(answer.empty() || answer.back()[1] <interval[0]){
+                answer.push_back(interval);
+            }
+            else{
+                answer.back()[1] = max(answer.back()[1], interval[1] );
+            }
+        }
+
+
+        return answer;
+    }
+};
