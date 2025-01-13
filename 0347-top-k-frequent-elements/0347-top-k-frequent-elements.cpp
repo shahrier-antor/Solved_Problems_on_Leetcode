61919eb96c7aef382c85a1bc4e4c8b73df68f4c1
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-13 14:13:00 +0200

    Time: 40 ms (5.34%), Space: 73.7 MB (5.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,25 +1,60 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+//         unordered_map<int,int>h_map;
+//         priority_queue<pair<int,int>>pq;
+//         vector<int>answer;
+//         for(int i:nums){
+//             h_map[i]++;
+//         }
+//         for(auto& [key,value]:h_map){
+//             pq.push({value,key});
+//         }
+        
+//         while(k-- > 0){
+//             int key = pq.top().second;
+//             answer.push_back(key);
+//             pq.pop();
+//         }
+
+//         return answer;
+
+    
+//     }
+// };
+
+// 2 -- 3
+// 3 -- 3
+
+// 3
+// {2,3}
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         unordered_map<int,int>h_map;
-        priority_queue<pair<int,int>>pq;
         vector<int>answer;
-        for(int i:nums){
-            h_map[i]++;
+        vector<queue<int>>bucket(nums.size()+1);
+
+        for(int i : nums)h_map[i]++;
+
+        for(auto& [key,freq]: h_map){
+            bucket[freq].push(key);
         }
-        for(auto& [key,value]:h_map){
-            pq.push({value,key});
+
+        for(int i = bucket.size()-1; i>=0 && answer.size()<k ; i--){
+
+            while(!bucket[i].empty() && answer.size()<k)
+            {
+                answer.push_back(bucket[i].front());
+                bucket[i].pop();
+            }
         }
+
         
-        while(k-- > 0){
-            auto key = pq.top();
-            answer.push_back(key.second);
-            pq.pop();
-        }
 
-        return answer;
-
-    
+        return answer; 
     }
 };
