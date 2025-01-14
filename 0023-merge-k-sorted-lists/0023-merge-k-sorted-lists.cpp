ebd8c7f0dc2ad7ad4fddde2307deed20a9516578
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-14 16:49:47 +0200

    Time: 11 ms (24.46%), Space: 18.6 MB (49.64%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,26 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+//         vector<int>solve;
+//         ListNode head(0);
+//         ListNode* current = &head;
+        
+//         for(auto node: lists){
+//             while(node){
+//                 solve.push_back(node->val);
+//                 node = node->next;
+//             }
+//         }
+//         sort(solve.begin(), solve.end());
+
+//         for(auto val:solve){
+//             current->next = new ListNode(val);
+//             current = current->next;
+//         }
+//        return head.next;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        vector<int>solve;
-        ListNode head(0);
-        ListNode* current = &head;
+        auto comparator = [](ListNode* a, ListNode*b){return a->val > b->val; };
+        priority_queue <ListNode*, vector<ListNode*>, decltype(comparator)> minHeap(comparator);
         
-        for(auto node: lists){
-            while(node){
-                solve.push_back(node->val);
-                node = node->next;
-            }
+        ListNode dummy(0);
+        ListNode* current = &dummy;
+        
+        for(auto list: lists){
+            if(list) minHeap.push(list);
         }
-        sort(solve.begin(), solve.end());
-
-        for(auto val:solve){
-            current->next = new ListNode(val);
+        while(!minHeap.empty()){
+            ListNode* temp = minHeap.top();
+            minHeap.pop();
+            current->next = temp;
             current = current->next;
+            if(temp->next)minHeap.push(temp->next);
         }
-       return head.next;
+        return dummy.next;
     }
 };
