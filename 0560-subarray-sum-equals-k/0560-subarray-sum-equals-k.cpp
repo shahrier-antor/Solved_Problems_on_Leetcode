c8ab0e54dee34c043e1fdcda3b1c825938729f65
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-18 15:52:28 +0200

    Time: 43 ms (68.93%), Space: 51 MB (24.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,37 @@
+// class Solution {   
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int high = 0, sum = 0, count=0;
+//         unordered_map<int,int>h_map;
+
+//         while(high<nums.size()){
+//             sum+=nums[high];
+//             if(sum == k) count++;
+//             count+= h_map[sum-k];
+//             h_map[sum]++;
+//             high++;
+//         }
+//         return count;
+//     }
+// };
+
+
+
+
 class Solution {   
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int high = 0, sum = 0, count=0;
-        unordered_map<int,int>h_map;
+    int subarraySum(vector<int>&nums, int k){
+        int high = 0, sum = 0,answer =0;
+        unordered_map<int,int> h_map;
 
         while(high<nums.size()){
             sum+=nums[high];
-            if(sum == k) count++;
-            count+= h_map[sum-k];
+            if(sum==k) answer++;
+            answer+=h_map[sum-k];
             h_map[sum]++;
             high++;
         }
-        return count;
+       return answer;
     }
+
 };
