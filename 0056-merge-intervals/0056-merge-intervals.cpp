1c1153c0609bce91a6797a3ddf42f5689a189940
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-18 17:11:12 +0200

    Time: 5 ms (56.49%), Space: 23.8 MB (54.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,19 +1,44 @@
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(),intervals.end());
+//         vector<vector<int>>answer;
+
+//         for(auto interval:intervals){
+//             if(answer.empty() || answer[answer.size()-1][1] <interval[0]){
+//                 answer.push_back(interval);
+//             }
+//             else{
+//                 answer[answer.size()-1][1] = max(answer[answer.size()-1][1], interval[1] );
+//             }
+//         }
+
+
+//         return answer;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>answer;
-
-        for(auto interval:intervals){
-            if(answer.empty() || answer.back()[1] <interval[0]){
-                answer.push_back(interval);
-            }
+       if(intervals.size()==1)return intervals;
+       sort(intervals.begin(), intervals.end());
+       int low = intervals[0][0], high = intervals[0][1];
+       vector<vector<int>>answer;
+       
+       for(int i = 1; i< intervals.size(); i++){
+            if(intervals[i][0]<=high) high = max(high,intervals[i][1]);
             else{
-                answer.back()[1] = max(answer.back()[1], interval[1] );
+                answer.push_back({low,high});
+                low = intervals[i][0];
+                high = intervals[i][1];
             }
-        }
-
-
-        return answer;
+       }
+       
+       answer.push_back({low,high});
+       return answer;
     }
 };
