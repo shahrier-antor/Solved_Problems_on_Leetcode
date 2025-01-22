edd54ffcfc84150caedb2a1a028b2c6fbfc66e40
Md Kaf Shahrier <antor3036@gmail.com>
2025-01-23 00:02:04 +0200

    Time: 0 ms (100%), Space: 63.3 MB (24.04%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,37 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+        
+//       priority_queue<int, vector<int>, greater<int>> pq;
+//       for(auto num:nums){
+//         pq.push(num);
+//         if(pq.size()>k)pq.pop();
+//       }
+//       return pq.top();
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         
-      priority_queue<int, vector<int>, greater<int>> pq;
-      for(auto num:nums){
-        pq.push(num);
-        if(pq.size()>k)pq.pop();
-      }
-      return pq.top();
+        vector<int>answer(20001,0);
+        for(auto num:nums){
+            answer[num+10000]++;
+        }
+        int kthlarge;
+
+        for(int i = answer.size()-1; i>=0; i--){
+            if(answer[i]!=0) {
+                kthlarge = i-10000;
+                k-= answer[i];
+                }
+                if(k<=0) break;
+        }
+        return kthlarge;
         
     }
 };
