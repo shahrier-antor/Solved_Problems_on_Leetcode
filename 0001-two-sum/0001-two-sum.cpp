0e0084ae829d06c6057d404ec328fedeef403a48
Md Kaf Shahrier <antor3036@gmail.com>
2025-02-02 19:56:13 +0200

    Time: 83 ms (15.49%), Space: 14.2 MB (60.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,55 @@
+// class Solution {
+// public:
+//     vector<int>twoSum( vector<int>&nums, int target){
+//         unordered_map<int, int>indices_values;
+//         for(int i=0; i<nums.size();i++){
+//             int rest = target - nums[i];
+//             if( indices_values.find(rest) != indices_values.end()){
+//                 return {indices_values[rest],i};
+//             }
+//             else{
+//                 indices_values[nums[i]] = i;
+//             }
+//     }
+//     return {};
+// }
+// };
+
+
+
+// class Solution {
+// public:
+//     vector<int>twoSum(vector<int>&nums, int target){
+//         int high = 0;
+//         unordered_map<int, int>indices;
+        
+//         while(high<nums.size()){
+//             if(indices.find(target-nums[high])!= indices.end()) return {high, indices[target-nums[high]]};
+//             else indices[nums[high]] = high;
+//             high++;
+//         }
+//         return {};
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
-    vector<int>twoSum( vector<int>&nums, int target){
-        unordered_map<int, int>indices_values;
-        for(int i=0; i<nums.size();i++){
-            int rest = target - nums[i];
-            if( indices_values.find(rest) != indices_values.end()){
-                return {indices_values[rest],i};
+    vector<int>twoSum(vector<int>&nums, int target){
+        vector<int>answer;
+        for(int i = 0; i<nums.size(); i++){
+            for(int j = i+1; j<nums.size(); j++){
+                if(nums[i]+nums[j] == target){
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    break;
+                }
             }
-            else{
-                indices_values[nums[i]] = i;
-            }
+        }
+
+        return answer;   
     }
-    return {};
-}
-
-
 };
