ded38d98bb68b066d266c37f79b24e6d64cba357
Md Kaf Shahrier <antor3036@gmail.com>
2025-08-03 02:10:58 +0300

    Time: 7 ms (39.15%), Space: 17.8 MB (69.21%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -1,10 +1,7 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
-        hashMap = defaultdict(int)
-        
-        for i in range (0, len(nums)):
-            for j in range (nums[i][0], nums[i][1]+1):
-                if j not in hashMap:
-                    hashMap[j] = 1
-        
-        return len(hashMap)
+        points = set()
+        for num in nums:
+            for i in range(num[0], num[1] + 1):
+                points.add(i)
+        return len(points)
