c1dee374d90a780545b8b2636e62125e8c1ce0ba
Md Kaf Shahrier <antor3036@gmail.com>
2025-08-06 23:02:51 +0300

    Time: 2121 ms (5.41%), Space: 110.2 MB (15.41%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -2,13 +2,27 @@ from collections import defaultdict
 
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
-        freq = defaultdict(int)
         n = len(s)
+        freq = defaultdict(int)
 
         for size in range(minSize, maxSize + 1):
-            for i in range(n - size + 1):
-                substr = s[i:i + size]
-                if len(set(substr)) <= maxLetters:
-                    freq[substr] += 1
+            left = 0
+            right = 0
+
+            while right < n:
+                # Expand window
+                window_len = right - left + 1
+
+                if window_len < size:
+                    right += 1
+                elif window_len == size:
+                    substr = s[left:right + 1]
+                    if len(set(substr)) <= maxLetters:
+                        freq[substr] += 1
+                    left += 1
+                    right += 1
+                else:
+                    # if window got too big, move left forward
+                    left += 1
 
         return max(freq.values(), default=0)
