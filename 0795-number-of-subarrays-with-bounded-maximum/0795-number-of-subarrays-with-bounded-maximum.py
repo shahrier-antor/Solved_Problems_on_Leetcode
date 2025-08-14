5bcbc21520130a3be0932946c1723225dc1cff51
Md Kaf Shahrier <antor3036@gmail.com>
2025-08-15 01:28:24 +0300

    Time: 280 ms (5.37%), Space: 23.2 MB (100%) - LeetHub

--- 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.py
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.py
@@ -1,17 +1,18 @@
 
 class Solution:
-    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+    def maxK(self, nums:List[int], k:int):
+        left, right = 0,0
+        maxele = -1
         ans = 0
-        low = 0          # 1 past the last index where nums[i] > right
-        add = 0          # number of valid subarrays ending at previous index
-
-        for high, x in enumerate(nums):
-            if x > right:
-                low = high + 1   # window is broken
-                add = 0          # nothing valid ends here
-            elif x >= left:       # x is a "good" max, resets add
-                add = high - low + 1
-                ans += add
-            else:                 # x < left, extend previous valid runs
-                ans += add
+        while(right<len(nums)):
+            maxele = max(maxele, nums[right])
+            if maxele>k:
+                maxele=-1
+                left = right+1
+            ans+= (right-left+1)
+            right+=1
         return ans
+
+    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+        print (self.maxK(nums, right), self.maxK(nums,left-1))
+        return self.maxK(nums, right) - self.maxK(nums,left-1)
