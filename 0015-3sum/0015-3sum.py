8e67527b2004b86ffbdfce1ce88c0039a115e457
Md Kaf Shahrier <msr25011@student.mdu.se>
2026-03-26 00:58:45 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,38 +1,33 @@
 class Solution:
-    # nums = [-4,-1,-1,0,1,2]
-
-
-
     def threeSum(self, nums):
-        results = []
         nums.sort()
+        result = []
 
         for i in range(len(nums)):
+            # skip duplicate i
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            j = i + 1
-            k = len(nums) - 1
+            left = i + 1
+            right = len(nums) - 1
 
-            while j < k:
-                total = nums[i] + nums[j] + nums[k]
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
 
-                if total < 0:
-                    j += 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                elif total > 0:
-                    k -= 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
+                if s == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                elif s < 0:
+                    left += 1
                 else:
-                    results.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
+                    right -= 1
 
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
+        # remove duplicates at the end
+        unique = []
+        for triplet in result:
+            if triplet not in unique:
+                unique.append(triplet)
 
-        return results
+        return unique
