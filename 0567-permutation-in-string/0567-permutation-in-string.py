9aadd843cd3a2deffddb6b7a28ae5e7f7164c571
Md Kaf Shahrier <msr25011@student.mdu.se>
2026-04-03 02:49:13 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -7,17 +7,14 @@ class Solution:
         freq_s2 = {}
 
         for right in range(len(s2)):
-            # Add current character
             freq_s2[s2[right]] = freq_s2.get(s2[right], 0) + 1
 
-            # Shrink window if too big
             if right - left + 1 > len(s1):
                 freq_s2[s2[left]] -= 1
                 if freq_s2[s2[left]] == 0:
-                    del freq_s2[s2[left]]
+                   del freq_s2[s2[left]]
                 left += 1
 
-            # Check when window size matches
             if right - left + 1 == len(s1):
                 if freq_s1 == freq_s2:
                     return True
